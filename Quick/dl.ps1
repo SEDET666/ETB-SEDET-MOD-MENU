2799cc3794396626aa88333844e9a62928bb8c23
SEDET666 <248881284@qq.com>
2026-03-27 08:17:20 +0800

    Update dl.ps1

--- Quick/dl.ps1
+++ Quick/dl.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -Uri "https://gitee.com/sedet/SEMOD-CheckUpdate/raw/master/QuickIns.zip" -OutFile "$env:TEMP\semodq.zip"
-Expand-Archive -Path "%temp%\semodq.zip" -DestinationPath "$env:TEMP\semod_q"
+Expand-Archive -Path "$env:TEMP\semodq.zip" -DestinationPath "$env:TEMP\semod_q"
 Invoke-WebRequest -Uri "https://gitee.com/sedet/SEMOD-CheckUpdate/raw/master/SEMODG_P.pak" -OutFile "$env:TEMP\semod_q\SEMOD_P.pak"
 & "$env:TEMP\semod_q\QuickIns.exe"
