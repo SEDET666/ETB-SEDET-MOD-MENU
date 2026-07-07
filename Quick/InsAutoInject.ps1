15a75ab5e21f9dc51c6238b4ef8f1994d99bf9ea
SEDET666 <248881284@qq.com>
2026-07-07 13:53:39 +0800

    Update InsAutoInject.ps1

--- Quick/InsAutoInject.ps1
+++ Quick/InsAutoInject.ps1
@@ -1,2 +1,6 @@
+Write-Output "SEMOD_EX has been discontinued. "
+Start-Sleep -s 3
+exit
+
 Invoke-WebRequest -Uri "https://github.com/SEDET666/ETB-SEDET-MOD-MENU/raw/refs/heads/main/Quick/SEMOD_EX.zip" -OutFile "$env:TEMP\semodex.zip"
 Expand-Archive -Path "$env:TEMP\semodex.zip" -DestinationPath "C:\SEMOD_EX"
